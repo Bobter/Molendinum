f70f215e19ce29307344cdc9a9ec2a9c79e7b102
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-05-07 15:33:27 -0500

    avance ficha
    creación de script ficha y avance

---
+++ Assets/Scripts/Tokens.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Tokens : MonoBehaviour
+{
+    Color playerColor;
+    //MeshRenderer meshColor;
+
+    public int playerIndex;//indice del jugador que usará la ficha
+    public int tokenIndex;//indice del la posición de la ficha
+    public GameObject SelectionEfect;//efecto que se activará al seleccionar ficha
+    public bool isSelected;//está o no está seleccionado la ficha la ficha 
+    public bool isActivate;//está o no está activado la ficha 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Desactive();
+        SelectionEfect.SetActive(false);
+        isSelected = false;
+        if (playerIndex == 1)
+        {
+            playerColor = Color.black;
+        } else if (playerIndex == 2)
+        {
+            playerColor = Color.white;
+        }
+        gameObject.GetComponent<MeshRenderer>().material.color = playerColor;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Active();
+        }
+        else if (Input.GetKeyUp(KeyCode.F))
+        {
+            Desactive();
+        }
+    }
+    public void Selected()
+    {
+        isSelected = true;
+        SelectionEfect.SetActive(true);
+    }
+    public void NoSelected()
+    {
+        isSelected = false;
+        SelectionEfect.SetActive(false);
+    }
+    public void Active()//se activa cuando se selecciona por primera vez la casilla 
+    {
+        isActivate = true;
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+    public void Desactive()//estado inical de la ficha 
+    {
+        isActivate = true;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+    private void OnMouseOver()
+    {
+        SelectionEfect.SetActive(true);
+        Debug.Log("OVER");
+    }
+    private void OnMouseExit()
+    {
+        SelectionEfect.SetActive(false);
+        Debug.Log("Exit");
+    }
+
+}
