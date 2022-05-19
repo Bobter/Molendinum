ddab5525281f5546d0fa80f4bfcb0155841ab84d
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-05-19 01:10:18 -0500

    GameManager and Edit Token
    Se crearon las primeras variables y funciones de GamemManager y en Token se cambiaron los indices de los jugadores , ahora el indice 0 es para el jugador 1 y el indice 1 es para el jugador 2,además se creó la función de movimiento de ficha

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int currentPlayerIndex;
+    public bool twoPlayers=false;
+    public float maxDistance;
+
+    public GameObject box;
+    public GameObject token;
+    public bool TokeIsMoving=false;
+    int[] unplacedTokens = { 9, 9 };
+    int[] totalTokens = { 9, 9 };
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,54 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray direction = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(direction.origin, direction.direction * maxDistance, Color.cyan);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SelecObject();
+        }
+    }
+
+    public void NextTurn()
+    {
+        currentPlayerIndex = ((currentPlayerIndex +1)% 2);
+    }
+    public void SelecObject()//función de selección del objeto
+    {
+        RaycastHit hit;
+        Ray mouseDirection = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(mouseDirection.origin, mouseDirection.direction, out hit, maxDistance))//si el rayo choca con algo
+        {
+            if (hit.transform.CompareTag("token"))
+            {   
+                if (hit.transform.GetComponent<Token>().playerIndex==currentPlayerIndex)
+                {
+                    token = hit.transform.gameObject;
+                }
+                Debug.Log("ficha");
+            } else if (hit.transform.CompareTag("box") && token != null)
+            {
+                Debug.Log("mover a");
+                StartCoroutine(token.GetComponent<Token>().Move(hit.transform.position));
+                token = null;
+                box = null;
+
+            }
+            else
+            {
+                Debug.Log("nada");
+                token = null;
+                box = null;
+            }
+        }
+        else
+        {
+            Debug.Log("nada");
+            token = null;
+            box = null;
+        }
+
     }
 }
