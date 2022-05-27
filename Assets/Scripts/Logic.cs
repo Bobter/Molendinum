2a790ef091cd463fc8f7015152fd86003067845e
Bobter <renzo.oyola.r@uni.pe>
2022-05-27 12:58:57 -0500

    Update

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -9,30 +9,30 @@ public class Logic : MonoBehaviour
 {       
     //Definiendo MOLINOS y VECINOSD
     public List<Tuple<int, int, int>> Molindenum = new List<Tuple<int, int, int>>();
-    public int[,] Neighbour = {{1,9,0,0},
-                            {0,2,4,1},
-                            {1,14,2,2},
-                            {4,10,3,3},
-                            {1,3,5,7},
-                            {4,13,5,5},
-                            {7,11,6,6},
-                            {4,6,8,7},
-                            {7,12,8,8},
-                            {0,10,21,9},
-                            {3,9,11,18},
-                            {6,10,15,11},
-                            {8,13,17,12},
-                            {5,12,14,20},
-                            {2,13,23,14},
-                            {11,16,15,15},
-                            {15,17,19,16},
-                            {12,16,17,17},
-                            {10,19,18,18},
-                            {16,18,20,22},
-                            {13,19,20,20},
-                            {9,22,21,21},
-                            {19,21,23, 22},
-                            {14,22,23,23},
+    public int[,] Neighbour = {{1,9,0,0},   //0
+                            {0,2,4,1},      //1
+                            {1,14,2,2},     //2
+                            {4,10,3,3},     //3
+                            {1,3,5,7},      //4
+                            {4,13,5,5},     //5
+                            {7,11,6,6},     //6
+                            {4,6,8,7},      //7
+                            {7,12,8,8},     //8
+                            {0,10,21,9},    //9
+                            {3,9,11,18},    //10
+                            {6,10,15,11},   //11
+                            {8,13,17,12},   //12
+                            {5,12,14,20},   //13
+                            {2,13,23,14},   //14
+                            {11,16,15,15},  //15
+                            {15,17,19,16},  //16
+                            {12,16,17,17},  //17
+                            {10,19,18,18},  //18
+                            {16,18,20,22},  //19
+                            {13,19,20,20},  //20
+                            {9,22,21,21},   //21
+                            {19,21,23, 22}, //22
+                            {14,22,23,23},  //23
                             };  //Hay 24 posiciones
 
     //Obteniendo posiciones
@@ -67,15 +67,14 @@ public class Logic : MonoBehaviour
     //Funcion que comprueba si hay un Molino(3 en raya)
 
     //Entrada checkbox.currentIndex y Board 
-    public bool Mill(int position, Board boardN)
+    public bool Mill(int position, Board boardN, int CurrentPlayer)
     {
-        var i = boardN.Coordinates[position];
 
         foreach(var p in Molindenum)
         {
             if (position == p.Item1 || position == p.Item2 || position == p.Item3)
             {
-                if (boardN.Coordinates[p.Item1] == i && boardN.Coordinates[p.Item2] == i && boardN.Coordinates[p.Item2] == i) 
+                if (boardN.Checkbox[p.Item1].checkboxIndex == CurrentPlayer && boardN.Checkbox[p.Item2].checkboxIndex == CurrentPlayer && boardN.Checkbox[p.Item2].checkboxIndex == CurrentPlayer) 
                 {
                     return true;
                 }
@@ -85,9 +84,9 @@ public class Logic : MonoBehaviour
     }
 
     //Nos da un booleano que nos dice si una ficha se puede eliminar si es que no forma parte de un molino
-    public bool Remove(int BoardPos, Board boardL) 
+    public bool Remove(int BoardPos, Board boardL, int CurrentPlayer) 
     {
-        return !Mill(BoardPos, boardL);
+        return !Mill(BoardPos, boardL,CurrentPlayer);
     }
 
     //Comprueba un movimiento valido
