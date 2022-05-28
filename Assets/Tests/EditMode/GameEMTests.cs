a5bd829745fe893c152752bb113e344bc4eebb03
Overglitch_ <phmrlldz@gmail.com>
2022-05-27 23:00:38 -0500

    NUnit Test
    Folder update

---
+++ Assets/Tests/EditMode/GameEMTests.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class GameEMTests
+{
+    [Test]
+    public void Mill()
+    {
+
+    }
+
+}
---
+++ Assets/Tests/PlayMode/GamePMTests.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class GamePMTests
+{
+    [UnityTest]
+    public IEnumerator Mill()
+    {
+
+        yield return null;
+    }
+}
